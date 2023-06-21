915b20a6ebddc0467a802b09025c8f382e1742c1
Andrea Guarino <andrea.guarino@sonarsource.com>
2023-06-21 11:56:45 +0200

    buggy code

--- demo/__init__.py
+++ demo/__init__.py
@@ -5,3 +5,28 @@ def add(x, y):
 def mul(x, y):
     return x * y
 
+
+def foo(total, param, ls):
+    l = len(ls)
+    if l == 0:
+        return total / l + param
+
+def bounded_integer(collection):
+    a = len(collection)
+    # Note that we do not support Python's comparison chains yet. See DBD-423
+    if 1 < a and a < 3:
+        return 1 / (a - 2)
+
+
+def bounded_index_variable(x):
+    ls = [1, 2, 3]
+    i = len(x)
+    if i >= 3:
+        return ls[i]
+    
+
+def redundant_condition():
+    i = len(x)
+    if i > 0:
+        if i > 10:
+            ...
